781946a43cb4db696d298f0bacaf1e82558dd77a
Nadia Pavita Amelia <nadiapavita@gmail.com>
2026-07-27 23:15:48 +0700

    Update login.py

--- login.py
+++ login.py
@@ -5,11 +5,11 @@ import time
 active_sessions = {}
 
 def get_user_from_db(username):
-    client = MongoClient("mongodb://pavita_ro:readonly@localhost:27017/?authSource=admin") # mongo lokal
-    # client = MongoClient("mongodb+srv://masukannamatemananda:masukanpasswordtemananda@posindo.5juwxxh.mongodb.net/")
+    # client = MongoClient("mongodb://pavita_ro:readonly@localhost:27017/?authSource=admin") # mongo lokal
+    client = MongoClient("mongodb+srv://masukannamatemananda:masukanpasswordtemananda@posindo.5juwxxh.mongodb.net/")
     
-    # db = client["pos_indonesia"]
-    db = client["datapos"] # mongo lokal
+    db = client["pos_indonesia"]
+    # db = client["datapos"] # mongo lokal
     collection = db["datalogin"]
     user = collection.find_one({"username": username})
     return user  
@@ -22,7 +22,7 @@ def init_session_state():
         "show_forgot_password": False,
         "username": None,
         "role":None,
-        # "cabang": None, # mongo lokal ini dimatiin
+        "cabang": None, # mongo lokal ini dimatiin
         "token": None
     }
     for key, value in defaults.items():
@@ -35,7 +35,7 @@ def login(username, password):
         st.session_state.logged_in = True
         st.session_state.username = username
         st.session_state.role = user.get("role", "user")
-        # st.session_state.cabang = user.get("cabang") # mongo lokal ini nya dimatiin
+        st.session_state.cabang = user.get("cabang") # mongo lokal ini nya dimatiin
         st.session_state.token = generate_token(username)
         return True
     return False
@@ -81,6 +81,6 @@ def logout():
     st.session_state.logged_in = False
     st.session_state.username = None
     st.session_state.role = None
-    # st.session_state.cabang = None # mongo lokal dimatiin
+    st.session_state.cabang = None # mongo lokal dimatiin
     st.session_state.token = None
     st.rerun()
