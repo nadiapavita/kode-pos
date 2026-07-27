5fee58d25b440c68f5fd59703b7adf18c66b670d
Nadia Pavita Amelia <nadiapavita@gmail.com>
2026-07-27 22:26:49 +0700

    Swap MongoDB connection strings in login.py

--- login.py
+++ login.py
@@ -5,8 +5,8 @@ import time
 active_sessions = {}
 
 def get_user_from_db(username):
-    #client = MongoClient("mongodb://pavita_ro:readonly@localhost:27017/?authSource=admin")
-    client = MongoClient("mongodb+srv://masukannamatemananda:masukanpasswordtemananda@posindo.5juwxxh.mongodb.net/")
+    client = MongoClient("mongodb://pavita_ro:readonly@localhost:27017/?authSource=admin")
+    # client = MongoClient("mongodb+srv://masukannamatemananda:masukanpasswordtemananda@posindo.5juwxxh.mongodb.net/")
     
     db = client["pos_indonesia"]
     collection = db["datalogin"]
